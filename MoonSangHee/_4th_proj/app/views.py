0b0919942ace347e921631fbd0c5eee70fd1c8ff
SANGHEEMOON <mshhhhh0000@naver.com>
2025-08-02 00:03:34 +0900

    Feat: 메인페이지 작성

--- MoonSangHee/_4th_proj/app/views.py
+++ MoonSangHee/_4th_proj/app/views.py
@@ -3,6 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 
-    return render(request, 'layout/base.html')
-
-# _4th_proj\templates\layout\base.html
+    return render(request, 'layout/home.html')
