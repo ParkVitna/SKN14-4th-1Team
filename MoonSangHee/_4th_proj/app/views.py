c77af2b826c0db90e714a672b8ed2565a2c4d1da
SANGHEEMOON <mshhhhh0000@naver.com>
2025-08-02 02:11:30 +0900

    Feat: 검색, 추천 페이지 작성

--- MoonSangHee/_4th_proj/app/views.py
+++ MoonSangHee/_4th_proj/app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
 
-    return render(request, 'layout/home.html')
+    return render(request, 'app/home.html')
+
+def search(request):
+    # 검색 페이지
+    q = request.GET.get("q", "")
+    ctx = {"q": q}
+    return render(request, "app/search.html", ctx)
+
+def recommend(request):
+    # 추천(챗봇) 페이지
+    return render(request, "app/recommend.html")
