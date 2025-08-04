f5153c46f73c001c46fb9e1aadadcf394b9df1d9
박빛나 <>
2025-08-05 00:47:14 +0900

    Feat: uauth db세팅, models 01

--- ParkVitna/_4th_01_project/_4th_01_project/settings.py
+++ ParkVitna/_4th_01_project/_4th_01_project/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = '_4th_01_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nutriwisedb',
+        'USER' : 'django',
+        'PASSWORD' : 'django',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
--- ParkVitna/_4th_01_project/uauth/models.py
+++ ParkVitna/_4th_01_project/uauth/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
-# Create your models here.
+# 사용자 상세정보를 저장할 모델
+class UserDetail(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(null=True, blank=True)
+    sex = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')], null=True, blank=True)
+    is_pregnancy = models.BooleanField(default=False)
+    health_concerns = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+# 회원가입 시 사용할 폼
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    birthday = forms.DateField(label="Birthday", required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+    sex = forms.ChoiceField(choices=[('M', 'Male'), ('F', 'Female')], required=False)
+    is_pregnancy = forms.BooleanField(required=False)
+    health_concerns = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
