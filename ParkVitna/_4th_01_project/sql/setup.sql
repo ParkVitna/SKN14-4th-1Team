ddc0f07d3b868dc08e4bef0552c9452712a1bea2
ParkVitna <bbn960903@gmail.com>
2025-08-05 20:37:35 +0900

    Feat: 회원가입 후, 로그인 (settings.py에서 비밀번호 정책 주석한 상태)

--- ParkVitna/_4th_01_project/sql/setup.sql
+++ ParkVitna/_4th_01_project/sql/setup.sql
@@ -15,3 +15,12 @@ create database nutriwisedb character set utf8mb4 collate utf8mb4_unicode_ci;
 grant all privileges on nutriwisedb.* to 'django'@'%';
 
 flush privileges;
+
+-- app > models.py 작업 후
+-- python manage.py makemigrations uauth
+-- python manage.py migrate uauth
+-- python manage.py migrate
+
+-- last_login 컬럼을 NULL 허용으로 변경
+-- (기존에는 NOT NULL로 설정되어 있어, 로그인하지 않은 사용자도 last_login
+ALTER TABLE auth_user MODIFY last_login DATETIME NULL;
