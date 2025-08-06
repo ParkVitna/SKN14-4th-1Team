702bff9714ad5681ef865d778f94c938d10dcb6b
ParkVitna <bbn960903@gmail.com>
2025-08-06 19:19:53 +0900

    Feat: 회원가입 js 처리 #30

--- ParkVitna/_4th_01_project/uauth/views.py
+++ ParkVitna/_4th_01_project/uauth/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.db import transaction
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 from .models import UserForm, UserDetail
 
@@ -46,3 +48,15 @@ def signup(request):
         form = UserForm()
 
     return render(request, 'uauth/signup.html', {'form': form})
+
+
+def check_usermail(request):
+  """
+  회원가입시 email 중복여부를 검사하는 ajax처리 뷰함수
+  """
+  useremail = request.GET.get('email')
+  # email 사용가능 여부 
+  available = User.objects.filter(useremail=useremail).exists() == False
+  print(f"{useremail=}, {available=}")
+
+  return JsonResponse({'available': available})
